a18b0c0a630d99729a8619efe8923fa0dfcbe554
DrPawel <Paulnatoryt@gmail.com>
2025-12-01 22:10:50 +0100

    Initial Commit

---
+++ Ninja_Path/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ninja_Path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
